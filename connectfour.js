48ad1eea4fae21f751454e5382080f0a90752c30
somethinrother <jamesneilson4@gmail.com>
2017-12-28 15:01:28 -0500

    Create stacking chip function

--- connectfour.js
+++ connectfour.js
@@ -20,24 +20,27 @@ document.addEventListener('DOMContentLoaded', function() {
   container.addEventListener('click', function(e) {
     var clicked = e.target
     var clickedColumn = clicked.getAttribute('data-column');
-    var fullColumn = document.querySelectorAll(`[data-column="${clickedColumn}"]`);
-
+    var fullColumn = [].slice.call(document.querySelectorAll(`[data-column="${clickedColumn}"]`));;
+    var allClicked = fullColumn.filter(cell => cell.getAttribute("data-clicked") === 'true');
     if (clicked.classList.contains('slot') ) {
-      fullColumn.forEach(function(cell) {
-        if (cell.getAttribute('data-clicked') === "false" && cell.getAttribute('data-row') === "0") {
+      for (var i = 0; i < fullColumn.length; i++) {
+        cell = fullColumn[i];
+        if (cell.getAttribute('data-clicked') === "true" && cell.getAttribute('data-row') > 4) {
+          break;
+        } else if (cell.getAttribute('data-clicked') === "false" && cell.getAttribute('data-row') === "0") {
           cell.style.backgroundColor = "red";
-          cell.setAttribute("data-clicked", true)
-        } else if (cell.style.backgroundColor !== "") {
-          cellRow = cell.getAttribute('data-row');
-          cellColumn = cell.getAttribute('data-column');
-          newCell = document.querySelectorAll(`[data-row='${cellRow + 1}'], [data-column='${cellColumn}']`)
-          newCell[0].style.backgroundColor = "red";
+          cell.setAttribute("data-clicked", true);
+          break;
+        } else if (cell.getAttribute('data-clicked') === "true") {
+          fullColumn.filter(cell => cell.getAttribute("data-clicked") === 'true');
+          newCellRow = parseInt(allClicked[0].getAttribute('data-row')) + 1;
+          newCell = fullColumn.filter(cell => cell.getAttribute('data-row') === `${newCellRow}`)[0];
+          newCell.style.backgroundColor = "red";
+          newCell.setAttribute("data-clicked", true);
+          break;
         }
-      } );
-    };
-    // if (clicked.classList.contains('slot') ) {
-    //   clicked.style.backgroundColor = "red";
-    // }
+      }
+    }
   });
 
 
