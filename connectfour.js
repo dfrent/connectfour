9de80df1ab52f99d79094f64af24c1f5e2c945a6
somethinrother <jamesneilson4@gmail.com>
2017-12-28 12:53:20 -0500

    Added var assignations

--- connectfour.js
+++ connectfour.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   function gameBoard() {
-    container = document.querySelector('.container');
-    for (divCount = 0; divCount < 42; divCount++) {
+    var container = document.querySelector('.container');
+    for (var divCount = 42; divCount > 0; divCount--) {
+      var rowNum = 0;
       newDiv = document.createElement('div');
       container.appendChild(newDiv);
       newDiv.className = `slot${divCount + 1} slot`;
+      newDiv.setAttribute("data-");
     }
   }
 
