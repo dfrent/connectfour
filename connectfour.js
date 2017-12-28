5b9dab9ebb725569a1f4a91274538b2b8a432071
somethinrother <jamesneilson4@gmail.com>
2017-12-28 13:03:47 -0500

    Create row numbers

--- connectfour.js
+++ connectfour.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   function gameBoard() {
     var container = document.querySelector('.container');
+    var rowNum = 0;
     for (var divCount = 42; divCount > 0; divCount--) {
-      var rowNum = 0;
       newDiv = document.createElement('div');
       container.appendChild(newDiv);
       newDiv.className = `slot${divCount} slot`;
-      newDiv.setAttribute("data-row", `${rowNum}`);
       if (divCount % 7 === 0 && divCount !== 42) {
         rowNum++;
       }
+      newDiv.setAttribute("data-row", `${rowNum}`);
     }
   }
 
