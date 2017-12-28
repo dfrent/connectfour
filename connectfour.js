b23ef399c7ae460f4a8105df443e3e68a3e31be2
somethinrother <jamesneilson4@gmail.com>
2017-12-28 13:10:34 -0500

    Add column numbers to divs

--- connectfour.js
+++ connectfour.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   function gameBoard() {
     var container = document.querySelector('.container');
-    var rowNum = 0;
+    var rowNum = 5;
     for (var divCount = 42; divCount > 0; divCount--) {
       newDiv = document.createElement('div');
       container.appendChild(newDiv);
       newDiv.className = `slot${divCount} slot`;
       if (divCount % 7 === 0 && divCount !== 42) {
-        rowNum++;
+        rowNum--;
       }
       newDiv.setAttribute("data-row", `${rowNum}`);
+      newDiv.setAttribute("data-column", `${(divCount - 1) % 7}`);
     }
   }
 
