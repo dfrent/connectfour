7ec637ad546757284e6393bf22fa6fca3c8eb8b9
somethinrother <jamesneilson4@gmail.com>
2017-12-28 12:03:30 -0500

    Create divs for board with javascript

--- connectfour.js
+++ connectfour.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  function gameBoard() {
+    container = document.querySelector('.container');
+    for (divCount = 0; divCount < 42; divCount++) {
+      newDiv = document.createElement('div');
+      container.appendChild(newDiv);
+      newDiv.class = 'slot';
+    }
+  }
 
+  gameBoard();
+}, false);
